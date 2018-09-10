954623bfd138029d9c5b2e416e8058d44a5c757e
jiantao <jiantao@pp100.com>
2018-09-11 07:20:41 +0800

    配置组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header></Header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link :to="{name:'goods'}">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{name:'goods'}">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{name:'goods'}">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Header from './components/Header/Header';
+
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less" scoped>
+  .tab {
+    display: flex;
+    height: 40px;
+    width: 100%;
+    line-height: 40px;
+    .tab-item {
+      flex: 1;
+      text-align: center;
+    }
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Goods from '@/components/Goods/Goods';
+import Ratings from '@/components/Ratings/Ratings';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/goods', name: 'goods', component: Goods},
+    {path: '/ratings', name: 'ratings', component: Ratings}
   ]
-})
+});
